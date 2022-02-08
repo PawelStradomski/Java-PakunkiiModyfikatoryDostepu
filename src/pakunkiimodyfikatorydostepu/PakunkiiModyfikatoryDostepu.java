64c4b5b0ccb16c085f119b3f3534178e3e0d3c92
pawelstradomski <pawelstradomski@localhost>
2022-02-08 13:48:45 +0100

    PakunkiModyfikatoryDostepu

---
+++ src/pakunkiimodyfikatorydostepu/PakunkiiModyfikatoryDostepu.java
@@ -0,0 +1,22 @@
+
+package pakunkiimodyfikatorydostepu;
+/**
+ * 
+ * @author pawelstradomski
+ */
+
+public class PakunkiiModyfikatoryDostepu {
+
+    
+    public static void main(String[] args) {
+//        Test.a = 20;
+        System.out.println(Test.a);
+    }
+    
+}
+//class Test
+//{
+//    static int a = 12;
+//}
+// zeby te zmiennie mogly byc zaimportowane przez NewClass nalezalo stworzyc klase test
+// ktora jest publiczna plus zmienne np int tez beda publiczne
---
+++ src/pakunkiimodyfikatorydostepu/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author pawelstradomski
+ */
+package pakunkiimodyfikatorydostepu;
+
+
+public class Test 
+{
+    public static int a = 777999; // zeby moglo zaimportowac, nalezalo dopisac "public"
+}                               // poniewaz wszystkie zmienne np int nie byly public tylko pakunkowe
